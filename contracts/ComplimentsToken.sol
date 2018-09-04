4d18d6eb75b02cc74ba20cd590dd054ed20cfe50
Lei Yuan <info@sendcompliments.com>
2018-09-04 11:41:21 +0200

    2. Transfer Token

--- contracts/ComplimentsToken.sol
+++ contracts/ComplimentsToken.sol
@@ -1,14 +1,37 @@
 pragma solidity ^0.4.2;
 
 contract Compliments {
-  // Constructor
-  // Set the total number of Tokens
-  // Read the total number of tokens
+  string public name = "Compliments";
+  string public symbol = "CPS";
+  string public standard = "Compliments v1.0";
   uint256 public totalSupply;
-  
-  function Compliments () public {
-    totalSupply = 54000000; // 54 million
 
+  event Transfer(
+    address indexed _from,
+    address indexed _to,
+    uint256 _value
+  );
 
+  mapping(address => uint256) public balanceOf;
+
+  function Compliments (uint256 _initialSupply) public {
+
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply; // 54 million
+
+  }
+
+  //Tranfer function
+  function transfer(address _to, uint256 _value) public returns (bool success) {
+    //Exception handling
+    require(balanceOf[msg.sender] >= _value);
+    // Transfer the balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    //Transfer event
+
+    Transfer(msg.sender, _to, _value);
+    // Boolean return
+    return true; 
   }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var Compliments = artifacts.require("./Compliments.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(Compliments);
+  deployer.deploy(Compliments, 54000000);
 };
